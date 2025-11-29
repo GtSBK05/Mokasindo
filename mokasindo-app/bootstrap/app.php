8acd457412c0fea11dcfc557136b7524798b3783
Fauzan Indarwan Zacky <143932642+FauzanIndarwan@users.noreply.github.com>
2025-11-29 10:43:19 +0700

    Add CSRF token validation exceptions

--- mokasindo-app/bootstrap/app.php
+++ mokasindo-app/bootstrap/app.php
@@ -11,6 +11,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->validateCsrfTokens(except: [
+            'etalase/*',
+            'wishlists/*',
+            'force-login', 
+        ]);
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
